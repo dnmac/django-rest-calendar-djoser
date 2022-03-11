b8ba99aeb8c922e2991a2cdbe585fe1b9245acd2
unknown <daniel-mcdermott@hotmail.com>
2022-03-11 10:13:52 +0000

    Postgres db removed SQL lite added back in.

--- project/project/settings.py
+++ project/project/settings.py
@@ -87,40 +87,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-
     'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'OPTIONS' : {
-                'options': '-c search_path=django,public'
-            },
-            'NAME': 'auth_system',
-            'USER': 'postgres',
-            'PASSWORD': 'password',
-            'HOST': 'db',
-            'PORT': '5432',
-    },
-
-    'vitfor': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'OPTIONS' : {
-                'options': '-c search_path=vitfor,public'
-            },
-            'NAME': 'auth_system',
-            'USER': 'postgres',
-            'PASSWORD': 'password',
-            'HOST': 'db',
-            'PORT': '5432',
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 
 # Password validation
